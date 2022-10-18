34de99c224662ffd8bb2c19117400165085feac6
Nicholas Meyers <nmeyers2899@gmail.com>
2022-10-18 13:55:38 -0500

    Added a Main Menu and a Level Select Menu to Begin at Different Levels

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerBehavior : MonoBehaviour
 {
@@ -19,11 +20,56 @@ public class GameManagerBehavior : MonoBehaviour
     [Tooltip("Checks to see every box in the scene has its on material.")]
     private bool _levelCompleted = false;
 
+    /// <summary>
+    /// Loads the next level in the game.
+    /// </summary>
+    public static void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Loads the menu where the player can choose which level to go into.
+    /// </summary>
+    public static void LoadSceneMenu()
+    {
+        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings - 1);
+    }
+
+    /// <summary>
+    /// Loads a given level.
+    /// </summary>
+    /// <param name="levelID"> The ID of the level to be loaded. </param>
+    public static void LoadLevel(int levelID)
+    {
+        SceneManager.LoadScene(levelID);
+    }
+
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
+    public static void QuitGame()
+    {
+        Debug.Log("Game Quit");
+
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Loads the first scene in the build order.
+    /// </summary>
+    public static void LoadStartScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     private void Update()
     {
+        // If the level is completed...
         if (_levelCompleted)
         {
-            _winScreen.enabled = true;
+            // ...let the win screen pop up.
+            _winScreen.gameObject.SetActive(true);
             return;
         }
 
