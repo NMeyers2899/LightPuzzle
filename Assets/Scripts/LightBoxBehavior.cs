b05e685e8b979fd89f8195894eb74e553538af63
Nicholas Meyers <nmeyers2899@gmail.com>
2022-10-18 10:36:44 -0500

    Created Boxes that Change Their Material on Click and Shaders

---
+++ Assets/Scripts/LightBoxBehavior.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBoxBehavior : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The material that represents the light being turned off.")]
+    private Material _offMaterial;
+
+    [SerializeField]
+    [Tooltip("The material that represents the light being turned on.")]
+    private Material _onMaterial;
+
+    [Tooltip("The mesh renderer for the object.")]
+    private MeshRenderer _meshRenderer;
+
+    [SerializeField]
+    [Tooltip("Determines whether or not the light is on or off.")]
+    private bool _lightIsOn;
+
+    [SerializeField]
+    [Tooltip("The radius aroudn the box that will determine its neighbors.")]
+    private RadiusBehavior _radius;
+
+    /// <summary>
+    /// Turns the light on or off depending on its current state.
+    /// </summary>
+    public void ToggleLight()
+    {
+        _lightIsOn = !_lightIsOn;
+    }
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if(_lightIsOn)
+            _meshRenderer.material = _onMaterial;
+        else
+            _meshRenderer.material = _offMaterial;
+    }
+
+    private void OnMouseDown()
+    {
+        // Toggles this box's light.
+        ToggleLight();  
+
+        // Toggles the nearby boxes' lights.
+        _radius.ToggleNearbyLights();
+    }
+}
---
+++ Assets/Scripts/RadiusBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadiusBehavior : MonoBehaviour
+{
+    [Tooltip("The nearby boxes that this can influence.")]
+    private List<RadiusBehavior> _nearbyBoxes;
+
+    [SerializeField]
+    [Tooltip("The box this radius is attached to.")]
+    private LightBoxBehavior _attachedBox;
+
+    /// <summary>
+    /// The nearby boxes that this can influence.
+    /// </summary>
+    public List<RadiusBehavior> NearbyBoxes { get { return _nearbyBoxes; } }
+
+    /// <summary>
+    /// The box this radius is attached to.
+    /// </summary>
+    public LightBoxBehavior AttachedBox { get { return _attachedBox; } }
+
+    private void Awake()
+    {
+        _nearbyBoxes = new List<RadiusBehavior>();
+    }
+
+    /// <summary>
+    /// Turns each light nearby this light on or off depending on its current state.
+    /// </summary>
+    public void ToggleNearbyLights()
+    {
+        // Toggles the light for each nearby box.
+        foreach (RadiusBehavior box in _nearbyBoxes)
+            box.AttachedBox.ToggleLight();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RadiusBehavior otherRadius;
+
+        // If the other object is another radius...
+        if(otherRadius = other.GetComponent<RadiusBehavior>())
+            // ...add the object to the list of other boxes.
+            _nearbyBoxes.Add(otherRadius);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        RadiusBehavior otherRadius;
+
+        // If the other object is another radius...
+        if (otherRadius = other.GetComponent<RadiusBehavior>())
+            // ...remove the object to the list of other boxes.
+            _nearbyBoxes.Remove(otherRadius);
+    }
+}
